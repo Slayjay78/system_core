b5cf9aa51743adaca0889a29c86a4792661ca9ca
Steve Kondik <steve@cyngn.com>
2015-09-08 02:23:41 -0700

    cutils: Add support for BFQIO cgroups
     * Add support for a custom hierarchy of cgroups on top of the BFQ
       IO scheduler. This allows us to place every thread into the
       right class (realtime/best-effort/idle) with a set up priority
       buckets depending on use case.
     * The benefit of doing this is pretty incredible from an
       interactivity standpoint. Realtime users (display/audio) benefit
       the most, resulting in glitch-free audio and jank-free video.
       Dexopting in the background no longer causes active harm to
       foreground tasks. Other tasks such as account syncing become
       invisible from the user's perspective.
     * Magic bullet? Perhaps.
    
    Change-Id: I4eb911395364ce46d6dcbff43e94286ded03a97d
    
    Conflicts:
    	libutils/Threads.cpp

--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -42,6 +42,7 @@
 #include <utils/threads.h>
 #include <utils/Log.h>
 
+#include <cutils/iosched_policy.h>
 #include <cutils/sched_policy.h>
 
 #ifdef HAVE_ANDROID_OS
@@ -95,6 +96,7 @@ struct thread_data_t {
             androidSetThreadName(name);
             free(name);
         }
+
         return f(u);
     }
 };
@@ -324,6 +326,8 @@ int androidSetThreadPriority(pid_t tid, int pri)
     } else {
         errno = lasterr;
     }
+
+    android_set_bfqio_prio(tid, pri);
 #endif
 
     return rc;
@@ -709,6 +713,12 @@ status_t Thread::run(const char* name, int32_t priority, size_t stack)
 
         return UNKNOWN_ERROR;
     }
+
+#ifdef HAVE_ANDROID_OS
+    pid_t tid = __pthread_gettid(android_thread_id_t_to_pthread(mThread));
+    if (tid >= 200)
+        android_set_bfqio_prio(tid, priority);
+#endif
 
     // Do not refer to mStatus here: The thread is already running (may, in fact
     // already have exited with a valid mStatus result). The NO_ERROR indication
